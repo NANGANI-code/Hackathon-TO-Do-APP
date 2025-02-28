c30a8ee477bab1a52a37ed75c975f7ef16ef8995
ellyjohnmwangi <johnmwangielly@gmail.com>
2025-03-01 00:46:22 +0300

    Business logic created

--- todo-app/src/main/java/com/example/todoapp/services/TaskService.java
+++ todo-app/src/main/java/com/example/todoapp/services/TaskService.java
@@ -1,9 +1,67 @@
-package com.example.todoapp.services;                   import org.springframework.stereotype.Service;
-
+package com.example.todoapp.services;
+import java.time.LocalDate;
+import java.util.*;
+import com.example.todoapp.datalayer.repository.TaskRepository;
+import com.example.todoapp.models.Task;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
 public class TaskService {
-    
-    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task task) {
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void markTaskAsCompleted(Long id){
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if(optionalTask.isPresent()){
+            Task task = optionalTask.get();
+            task.setCompleted(true);
+            taskRepository.save(task);
+        }
+    }
+
+    public List<Task> getCompletedTasks() {
+        return taskRepository.findByCompleted(true);
+    }
+
+    public List<Task> getPendingTasks() {
+        return taskRepository.findByCompleted(false);
+    }
+
+    public List<Task> getHighPriorityTasks() {
+        return taskRepository.findByPriority("High");
+    }
+
+    public List<Task> getOverdueTasks() {
+        return taskRepository.findByDueDateBefore(LocalDate.now());
+    }
+
+    public List<Task> getTasksDueOn(LocalDate dueDate) {
+        return taskRepository.findByDueDate(dueDate);
+    }
+
 }
+
+
